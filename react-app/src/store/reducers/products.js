c12487be611e5f331c7796f35cfcb2ff276f05a6
Cynthia Spence <cynthiaspence7827@gmail.com>
2020-12-13 14:19:37 -0700

    product reducer

--- react-app/src/store/reducers/products.js
+++ react-app/src/store/reducers/products.js
@@ -1,4 +1,4 @@
-import { SET_PRODUCTS } from "../actions/products";
+import { SET_ALL_PRODUCTS, EDIT_PRODUCT, ADD_PRODUCT, DELETE_PRODUCT } from "../actions/products";
 
 const initialState = {
     dict: {},
@@ -9,9 +9,19 @@ export default function reducer(state = initialState, action) {
     let newState = { ...state };
 
     switch (action.type) {
-        case SET_PRODUCTS:
-            newState.dict = action.products.dict;
-            newState.ids = action.products.ids;
+        case SET_ALL_PRODUCTS:
+            newState = action.products;
+            return newState;
+        case EDIT_PRODUCT:
+            newState.dict[action.product.id] = action.product;
+            return newState;
+        case ADD_PRODUCT:
+            newState.dict[action.product.id] = action.product;
+            return newState;
+        case DELETE_PRODUCT:
+            delete newState.dict[action.productId];
+            newState.ids.filter(id => id !== action.productId);
+            return newState;
         default:
             return state;
     }
